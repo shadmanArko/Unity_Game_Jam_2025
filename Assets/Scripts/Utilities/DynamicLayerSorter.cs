ad25580183d4168cb43f8d123151f8bdfbbf9c58
Samiul Islam <samiulislamshad@gmail.com>
2025-11-09 01:33:21 +0600

    player animation and dynamic sorting

---
+++ Assets/Scripts/Utilities/DynamicLayerSorter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class DynamicLayerSorter : MonoBehaviour
+    {
+        [Header("Sorting Settings")]
+        [SerializeField] private int sortingOffset;
+        [SerializeField] private float yMultiplier = 1000f; // Higher = more precise sorting differences
+
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        
+        private void LateUpdate()
+        {
+            spriteRenderer.sortingOrder = -(int)(transform.position.y * yMultiplier) + sortingOffset;
+        }
+    }
+}
