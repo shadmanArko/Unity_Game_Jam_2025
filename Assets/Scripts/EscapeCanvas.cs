0f5f9168bd0499d67453f782ccc427c38545018f
Nuruzzaman Life <nhlife2302@gmail.com>
2025-11-08 15:13:05 +0600

    game scene updated

---
+++ Assets/Scripts/EscapeCanvas.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EscapeCanvas : MonoBehaviour
+{
+    [SerializeField] private GameObject escapeCanvasPanel;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button endRunButton;
+    [SerializeField] private Button exitGameButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isPanelActive = false;
+    void Start()
+    {
+        resumeButton.onClick.AddListener(ResumeGame);
+        endRunButton.onClick.AddListener(EndRunAndRestartGame);
+        exitGameButton.onClick.AddListener(ExitGame);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void EndRunAndRestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void ResumeGame()
+    {
+        escapeCanvasPanel.SetActive(false);
+        isPanelActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Escape))
+        // {
+        //     escapeCanvasPanel.SetActive(!isPanelActive);
+        //     isPanelActive = !isPanelActive;
+        // }
+    }
+}
